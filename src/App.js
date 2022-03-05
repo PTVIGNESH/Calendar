0e92162b411747bd867e000250c4ebe222403662
Vigneshwaran <vigneshwaranp2014@gamil.com>
2022-03-05 11:47:23 +0530

    feat: prime react (_)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
+import { Card } from 'primereact/card';
 import './App.css';
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
 
 function App() {
   return (
     <div>
       <header>
-        <h1>Hello</h1>
+        <h1>Hello World</h1>
+        <Card title="Title" subTitle="SubTitle">
+              Content
+        </Card>
       </header>
     </div>
   );
