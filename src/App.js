aec3473e07f06a2067b8e66adcb05b98326de2a8
Vigneshwaran <vigneshwaranp2014@gamil.com>
2022-03-06 16:09:53 +0530

    fnished in Assignment

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {Calendar} from '@fullcalendar/core';
 import dayGridPlugin from "@fullcalendar/daygrid";
 import  timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import Footer from './Footer';
 
 function App() {
   const options = {
@@ -40,10 +41,61 @@ function App() {
       author: "PTVignesh",
       host: "Nick",
       title: "this link share to hello@pamper.com",
-
       
         start: "2022-03-07T17:00:00",
         end: "2022-03-07T19:00:00",
+  
+
+    time: {
+       start: "0800",
+       end: "1000",
+       timezone: "UTC",
+     },
+
+     description: "Event description",
+
+    },
+
+    {
+      type: "TwitterSpace", //Live Stream
+      location: {
+        type: "virtual", //India 
+        address: "India",
+      },
+
+      url: "http://github.com/PTVIGNESH/EventCalendar",
+      author: "PTVignesh",
+      
+      title: "Gmail...send",
+      
+        start: "2022-03-09T17:00:00",
+        end: "2022-03-09T19:00:00",
+  
+
+    time: {
+       start: "0800",
+       end: "1000",
+       timezone: "UTC",
+     },
+
+     description: "Event description",
+
+    },
+  
+    {
+      type: "TwitterSpace", //Live Stream
+      location: {
+        type: "virtual", //India 
+        address: "India",
+      },
+
+      url: "http://github.com/PTVIGNESH/EventCalendar",
+      author: "PTVignesh",
+      
+      title: "Sec. Round",
+      
+        start: "2022-03-09T17:00:00",
+        end: "2022-03-09T19:00:00",
   
 
     time: {
@@ -61,15 +113,16 @@ function App() {
 
      <Home />
       <header> 
-         <Menubar start=
+         <Menubar  className='menubar' start=
                    {
-                   <InputText placeholder="Search" 
-            type="text" />
+                   <InputText className='inputtext' placeholder="Search" 
+            type="text"  />
             } />
          <h1 className='title'>Event Calendar</h1>
       </header>
         <FullCalendar className='fullcalendar' events={events} options={options} />
         
+        <Footer />
     </div>
   );
 }
